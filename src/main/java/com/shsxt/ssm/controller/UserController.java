62e7797a3aeb2f543c0d552fd216edd6f938bccf
tony <tony.kang@indulgesmart.com>
2016-08-20 08:42:51 +0800

    modify

--- src/main/java/com/shsxt/ssm/controller/UserController.java
+++ src/main/java/com/shsxt/ssm/controller/UserController.java
@@ -14,7 +14,7 @@ import com.shsxt.ssm.service.UserService;
 @RequestMapping("/user")
 public class UserController {
 	
-	@Autowired
+	@Autowired  
 	private UserService userService;
 	
 	@RequestMapping("/find")
